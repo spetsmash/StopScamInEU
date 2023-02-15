93fdd6789ce218648c3c781b55c666060ca41aff
t096509 <t0965097707@gmail.com>
2023-02-15 20:14:12 +0100

    Dobavil neskolko variantov dlia Nordbau

--- blackList/companiesCheatPartners.js
+++ blackList/companiesCheatPartners.js
@@ -1,12 +1,12 @@
 export const companiesCheatPartners = [
     /*{
-        title: "Mk Bau",
-        search: ["mk bau", "mkbau", "MK Bau"],
+        title: "MK Bau",
+        search: ["mk bau", "mkbau", "MK Bau", "MK bau"],
         taxNumber: "209206852"
     },*/
     {
         title: "Nordbau Marandi",
-        search: ["Nordbau Marandi", "Nordbau"],
+        search: ["Nordbau Marandi", "Nordbau", "nordbau", "Nordbau marandi", "nordbau marandi"],
         taxNumber: "unknown"
     }
 ];
