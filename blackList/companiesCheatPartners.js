682f10a391125812fae9c32da6d89e14972ca226
t096509 <t0965097707@gmail.com>
2023-02-15 20:09:21 +0100

    MK Bau zakomentil

--- blackList/companiesCheatPartners.js
+++ blackList/companiesCheatPartners.js
@@ -1,13 +1,13 @@
 export const companiesCheatPartners = [
-    {
+    /*{
         title: "Mk Bau",
         search: ["mk bau", "mkbau", "MK Bau"],
         taxNumber: "209206852"
-    },
+    },*/
     {
         title: "Nordbau Marandi",
         search: ["Nordbau Marandi", "Nordbau"],
-        taxNumber: ""
+        taxNumber: "unknown"
     }
 ];
 
