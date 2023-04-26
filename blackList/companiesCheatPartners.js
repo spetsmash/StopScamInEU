8beaeed902929b592aa4a3150be528edfa02a1c0
t096509 <t0965097707@gmail.com>
2023-04-26 16:01:14 +0200

    Dobavil "MK Bau"

--- blackList/companiesCheatPartners.js
+++ blackList/companiesCheatPartners.js
@@ -1,13 +1,18 @@
 export const companiesCheatPartners = [
-    /*{
+    {
         title: "MK Bau",
-        search: ["mk bau", "mkbau", "MK Bau", "MK bau"],
+        search: ["mk bau", "mkbau", "MK Bau", "MK bau", "MK BAU"],
         taxNumber: "209206852"
-    },*/
+    },
     {
         title: "Nordbau Marandi",
         search: ["Nordbau Marandi", "Nordbau", "nordbau", "Nordbau marandi", "nordbau marandi"],
         taxNumber: "unknown"
-    }
+    },
+    /*{
+     title: "MK Bau",
+     search: ["mk bau", "mkbau", "MK Bau", "MK bau"],
+     taxNumber: "209206852"
+ }*/
 ];
 
