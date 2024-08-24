f6ac7ebb79864d56220cceef5d364a48352a7e2b
Natali <16439120+spetsmash@users.noreply.github.com>
2024-08-24 00:37:37 -0400

    Update companiesCheatPartners.js

--- blackList/companiesCheatPartners.js
+++ blackList/companiesCheatPartners.js
@@ -9,6 +9,11 @@ export const companiesCheatPartners = [
         search: ["Nordbau Marandi", "Nordbau", "nordbau", "Nordbau marandi", "nordbau marandi"],
         taxNumber: "unknown"
     },
+    {
+        title: "Solarbringer GmbH",
+        search: ["Solarbringer GmbH", "Solarbringer", "solarbringer", "solarbringer gmbh"],
+        taxNumber: "357352196"
+    }
     /*{
      title: "MK Bau",
      search: ["mk bau", "mkbau", "MK Bau", "MK bau"],
